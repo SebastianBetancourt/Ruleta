89736df68018d4135459718d21a5a927045113b9
Sebastián <sebastian@debian>
2018-10-12 10:13:11 -0500

    primer commit

---
+++ src/Principal.java
@@ -0,0 +1,16 @@
+
+public class Principal {
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
